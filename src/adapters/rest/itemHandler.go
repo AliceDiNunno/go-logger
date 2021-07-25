cfb1f9d1e0ee974c1e16a192c3332f88d49d3891
Alice Di Nunno <alice@dinunno.fr>
2021-07-25 18:51:22 +0200

    Cleaned routes

---
+++ src/adapters/rest/itemHandler.go
@@ -0,0 +1,25 @@
+package rest
+
+import "github.com/gin-gonic/gin"
+
+func (rH RoutesHandler) fetchingGroupMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+	}
+}
+
+func (rH RoutesHandler) SearchGroupingIdsHandler(c *gin.Context) {
+
+}
+
+func (rH RoutesHandler) PushLogsHandler(c *gin.Context) {
+
+}
+
+func (rH RoutesHandler) SearchLogsInGroupingHandler(c *gin.Context) {
+
+}
+
+func (rH RoutesHandler) GetSpecificLogsHandler(c *gin.Context) {
+
+}
--- src/adapters/rest/routes.go
+++ src/adapters/rest/routes.go
@@ -11,22 +11,24 @@ func SetRoutes(r *gin.Engine, routesHandler RoutesHandler) {
 	r.GET("", routesHandler.GetAllAppsHandler) //Get all apps
 	r.POST("", routesHandler.CreateAppHandler) //Create an app
 
-	appGroup := r.Group(":id", routesHandler.fetchingAppMiddleware())
+	appGroup := r.Group(":app_id", routesHandler.fetchingAppMiddleware())
 	appGroup.GET("", routesHandler.GetAppHandler)       // Get an app
 	appGroup.DELETE("", routesHandler.DeleteAppHandler) //Delete an app
 
 	environmentGroup := appGroup.Group("/environment")
-	environmentGroup.GET("", nil) //Getting all declared environments for an app
+	environmentGroup.GET("", routesHandler.GetEnvironmentHandler) //Getting all declared environments for an app
 
 	versionGroup := appGroup.Group("/version")
-	versionGroup.GET("", nil) //Getting all declared version for an app
+	versionGroup.GET("", routesHandler.GetVersionHandler) //Getting all declared version for an app
 
 	serverGroup := appGroup.Group("/server")
-	serverGroup.GET("", nil) //Getting all declared servers for an app
+	serverGroup.GET("", routesHandler.GetServerHandler) //Getting all declared servers for an app
 
 	itemsGroup := appGroup.Group("/items")
-	itemsGroup.GET("", nil)    //Getting all grouping ids
-	itemsGroup.GET(":id", nil) //Retrieve all logs (corresponding to a grouping ID)
-	itemsGroup.POST("", nil)   //Push a log
+	itemsGroup.GET("", routesHandler.SearchGroupingIdsHandler) //Search all grouping ids
+	itemsGroup.POST("", routesHandler.PushLogsHandler)         //Push a log
 
+	logsGroup := itemsGroup.Group("/:grouping_id", routesHandler.fetchingGroupMiddleware())
+	logsGroup.GET("/", routesHandler.SearchLogsInGroupingHandler)   //Search all logs (corresponding to a grouping ID)
+	logsGroup.GET("/:log_id", routesHandler.GetSpecificLogsHandler) //Getting a specific log id
 }
